1acf6606b141a79a3b52b4060c266d187e1039da
mjt320 <36667519+mt@users.noreply.github.com>
2020-06-30 16:03:13 +0100

    Update MTSat_derivation.m
    add eqns from Helms et al. erratum

--- MTSat_derivation.m
+++ MTSat_derivation.m
@@ -20,16 +20,35 @@ Sol_Delta=solve([eqn_S_MT],[delta])
 
 syms T a
 
-exprHelm_A =  (((S_PD*a_PD)/TR_PD)/(S_T1*a_T1-S_PD*a_PD)) * (a_T1/a_PD - a_PD/a_T1); % Eqn 7b
-exprHelm_R1 = (S_T1*a_T1-S_PD*a_PD)/(2*T*(S_PD/a_PD-S_T1/a_T1)); % Eqn 7a
-exprHelm_delta = (A*(a/S_MT)-1)*R1*T - (a^2)/2; % Eqn 8
+exprHelms_A =  (((S_PD*a_PD)/TR_PD)/(S_T1*a_T1-S_PD*a_PD)) * (a_T1/a_PD - a_PD/a_T1); % Eqn 7b
+exprHelms_R1 = (S_T1*a_T1-S_PD*a_PD)/(2*T*(S_PD/a_PD-S_T1/a_T1)); % Eqn 7a
+exprHelms_delta = (A*(a/S_MT)-1)*R1*T - (a^2)/2; % Eqn 8
 exprLema_A = ((2*TR_PD)/a_PD^2 - ((2*TR_T1)/a_T1^2))/... % Eqn 3
     ( (2*TR_PD)/(a_PD*S_PD) - (2*TR_T1)/(a_T1*S_T1) );
 exprLema_T1 = (S_PD/a_PD - S_T1/a_T1) / ((S_PD*a_PD)/(2*TR_PD)) -... % Eqn 4
     (S_T1*a_T1)/(2*TR_T1);
+exprHelms_erratum_R1 = (1/2)*( ( (S_T1*a_T1)/TR_T1 - (S_PD*a_PD)/TR_PD ) /...
+    ( S_PD/a_PD - S_T1/a_T1 )); % Eqn 7a
+exprHelms_erratum_A = S_PD*S_T1*( ( (TR_PD*a_T1)/a_PD - (TR_T1*a_PD)/a_T1 ) /...
+    ( S_T1*TR_PD*a_T1 - S_PD*TR_T1*a_PD ) ); % Eqn 7b
 
-simplify(subs(Sol_A_R1.A,TR_T1,TR_PD)-exprHelm_A) %assume TR_T1=TR_PD; Helms equation for A (does not match)
-simplify(subs(Sol_A_R1.R1,[TR_PD,TR_T1],[T,T])-exprHelm_R1) %assume TR_PD=TR_T1=T,; Helms equation for R1 (matches)
-simplify(subs(Sol_Delta,[TR_PD,a_PD],[T,a])-exprHelm_delta) %Helm equation for delta (matches)
-simplify(Sol_A_R1.A-exprLema_A) %Lema equation for A (matches)
-simplify(Sol_A_R1.R1-1/exprLema_T1) %Lema equation for T1 (does not match)
+disp('assume TR_T1=TR_PD; Helms equation for A (does not match)');
+simplify(subs(Sol_A_R1.A,TR_T1,TR_PD)-exprHelms_A)
+
+disp('assume TR_PD=TR_T1=T,; Helms equation for R1 (matches)');
+simplify(subs(Sol_A_R1.R1,[TR_PD,TR_T1],[T,T])-exprHelms_R1) 
+
+disp('Helm equation for delta (matches)');
+simplify(subs(Sol_Delta,[TR_PD,a_PD],[T,a])-exprHelms_delta) 
+
+disp('Lema equation for A (matches)');
+simplify(Sol_A_R1.A-exprLema_A) 
+
+disp('Lema equation for T1 (does not match)');
+simplify(Sol_A_R1.R1-1/exprLema_T1) 
+
+disp('Helms erratum equation for A (matches)');
+simplify(Sol_A_R1.A-exprHelms_erratum_A) 
+
+disp('Helms erratum equation for T1 (matches)');
+simplify(Sol_A_R1.R1-exprHelms_erratum_R1) 
